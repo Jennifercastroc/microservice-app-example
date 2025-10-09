edd8f93ba00566882c6452533daabc1bed41dee7
JenniferCastrocd <1111540677@u.icesi.edu.co>
2025-10-08 19:18:46 -0500

    test

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -22,7 +22,7 @@
                                   type='text'
                                   name='username'
                                   class='form-control'
-                                  placeholder='xdxd whathiiii what what happened please'
+                                  placeholder='xdxd hiiii what what happened please'
                                   v-model='credentials.username'
                                   required
                                   autofocus
